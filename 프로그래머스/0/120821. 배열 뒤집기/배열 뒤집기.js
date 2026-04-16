ac4d39cfa6b86f770e61c42b504dd620c2b5b04f
way <127731509+sooloin@users.noreply.github.com>
2026-04-16 12:54:54 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,8 +1,3 @@
 function solution(num_list) {
-    const reverseNum = [];
-    
-    for (let i = num_list.length-1; i >= 0; i--) {
-        reverseNum.push(num_list[i]);
-    }
-    return reverseNum;
+    return num_list.reverse();
 }
